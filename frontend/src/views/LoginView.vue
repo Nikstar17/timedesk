6bf94f89720f60a8dce1ee4e1574d9b1fe8f3d46
Nik <dominik.k276@gmail.com>
2025-04-17 18:02:11 +0200

    feat: update API endpoint for login and add deployment workflows for frontend and backend

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -68,7 +68,7 @@ async function handleLogin() {
   error.value = ''
 
   try {
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('http://localhost:8000/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
